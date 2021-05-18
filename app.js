6d73cc5b07c0638797aa636fd61742a6ff46e4e8
Mastagoon <iznilagiway@gmail.com>
2021-05-18 15:08:51 +0300

    corrected some typos

--- app.js
+++ app.js
@@ -5,26 +5,18 @@ const config = require("./config.js")
 require("dotenv").config()
 const log = require("./utils/log.js")
 const getCommandName = require("./utils/getCommandName")
+const { getString } = require("./utils/lang")
 
-const admins = config.admins
-let adminCommands = [], commandAliases = []
 
 bot.commands = new Discord.Collection()
 
 const commandFiles = fs.readdirSync("./commands").filter(f => f.endsWith(".js"))
 
-const adminCommandFiles = fs.readdirSync("./adminCommands").filter(f => f.endsWith(".js"))
 
 for(const file of commandFiles) {
     const command = require(`./commands/${file}`)
     bot.commands.set(command.name, command)
     command.aliases.split(",").forEach(al => commandAliases.push({alias: al, cmd: command.name}))
-}
-for (const file of adminCommandFiles) {
-    const command = require(`./adminCommands/${file}`)
-    bot.commands.set(command.name, command)
-    command.aliases.split(",").forEach(al => commandAliases.push({alias: al, cmd: command.name}))
-    adminCommands.push(command.name)
 }
 
 bot.on("ready", () => {
@@ -40,12 +32,11 @@ bot.on("message", (message) => {
     const command = getCommandName(args.shift().toLowerCase(), commandAliases)
     if(!bot.commands.has(command)) return
     if(bot.commands.get(command).channels && !bot.commands.get(command).channels.includes(message.channel.id)) return // wrong channel
-    if(adminCommands.includes(command) && !config.admins.includes(message.author.id)) return
     try {
         bot.commands.get(command).execute(message, args, bot)
     } catch(err) {
-        utils.log(`main: error executing a command. ${err.message}`, "error")
-        message.reply(utils.getString(message.member, "commandError"))
+        log(`main: error executing a command. ${err.message}`, "error")
+        message.reply(getString(message.member, "commandError"))
     }
 })
 
