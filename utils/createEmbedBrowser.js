a1404fd858a001cb1d12b6b35e8fbdad31e63404
Mastagoon <inzilagiway@gmail.com>
2021-07-24 17:56:37 +0000

    fixed a bug that crashes the bot when using commands in a DM

--- utils/createEmbedBrowser.js
+++ utils/createEmbedBrowser.js
@@ -12,17 +12,17 @@ module.exports = async(data, message, userId, time = 120000) => {
     }
     let i = 0
     for(embed of embeds) {
-        embeds[i] = new Discord.MessageEmbed(embed).setFooter(`Page ${i+1}/${pages} | ${message.guild.me.hasPermission("MANAGE_MESSAGES") ? "Use the arrow emojis below to browse through the pages." : "Give me 'manage messages' permission so that i can do my job better."}`, "https://cdn.discordapp.com/attachments/844031096752570398/844330289622286356/logo_untrusted.png")
+        embeds[i] = new Discord.MessageEmbed(embed).setFooter(`Page ${i+1}/${pages} | ${message.guild?.me.hasPermission("MANAGE_MESSAGES") ? "Use the arrow emojis below to browse through the pages." : "Give me 'manage messages' permission so that i can do my job better."}`, "https://cdn.discordapp.com/attachments/844031096752570398/844330289622286356/logo_untrusted.png")
         i++
     }
     const listEmbed = new Discord.MessageEmbed(list).setFooter(`Index page | 
-    ${message.guild.me.hasPermission("MANAGE_MESSAGES") ? "Please enter a page number." : "Give me 'manage messages' permission so that i can do my job better"}`, "https://cdn.discordapp.com/attachments/844031096752570398/844330289622286356/logo_untrusted.png")
+    ${message.guild?.me.hasPermission("MANAGE_MESSAGES") ? "Please enter a page number." : "Give me 'manage messages' permission so that i can do my job better"}`, "https://cdn.discordapp.com/attachments/844031096752570398/844330289622286356/logo_untrusted.png")
     message.edit("", embeds[0])
     let index = 0
     for(emoji of emojis) await message.react(emoji)
     const filter = (r, u) => emojis.includes(r.emoji.name) && u.id === userId
     const reactionCollector = message.createReactionCollector(filter, { time })
-    if(message.guild.me.hasPermission("MANAGE_MESSAGES"))
+    if(message.guild?.me.hasPermission("MANAGE_MESSAGES"))
         reactionCollector.on(`end`,() => message.reactions.removeAll())
     reactionCollector.on(`collect`, (r,u) => {
         switch(emojis.indexOf(r.emoji.name)) {
@@ -57,7 +57,7 @@ module.exports = async(data, message, userId, time = 120000) => {
                 })
                 break
         }
-        if(message.guild.me.hasPermission("MANAGE_MESSAGES"))
+        if(message.guild?.me.hasPermission("MANAGE_MESSAGES"))
             r.users.remove(u.id)
     })
 }
