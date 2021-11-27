f3311a9af347b805452f5aeedd01de84212b0ee7
Mastagoon <inzilagiway@gmail.com>
2021-11-27 17:40:35 +0300

    fixed a bug with the latest changes

--- seeders/classDataSeeder.js
+++ seeders/classDataSeeder.js
@@ -5,11 +5,11 @@ const fs = require("fs")
 axios.get("https://www.playuntrusted.com/publicAPI/publicAPI.php?request=getClasses").then(res => {
     const { data } = res
     let regex = /\,(?!\s*?[\{\[\"\'\w])/g  // remove trailing commas
-    const classList = JSON.parse(data.replace(regex, '').replace('"', '\"'))
+    // const classList = JSON.parse(data.replace(regex, '').replace('"', '\"'))
     // add aliases
-    for(cl of classList) {
+    for(cl of data) {
         const alias = classAliases.find(al => Number(al.id) == Number(cl.id))
         cl["name"] = alias?.name
     }
-    fs.writeFileSync(`../data/classData.json`, JSON.stringify(classList,null,1))
+    fs.writeFileSync(`../data/classData.json`, JSON.stringify(data), null, 1)
 })
