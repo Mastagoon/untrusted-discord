5d4343bf18bf57eb90f7762d3a334651fe125ab1
Mastagoon <inzilagiway@gmail.com>
2023-03-12 15:06:08 +0300

    supporter role fixes

--- src/direct_commands/supporter.ts
+++ src/direct_commands/supporter.ts
@@ -5,17 +5,19 @@ import Log from "../utils/logger"
 const supporter = async (msg: Message, args: string[], bot: Client): Promise<Message<boolean>> => {
 	if (msg.channel.type !== 'DM') return msg.reply({ content: 'This command can only be used in DMs!' })
 	if (!args[0]) return msg.reply({ content: 'Please provide a supporter code!' })
+	if (!args[1]) return msg.reply({ content: 'Please provide your in-game nickname!' })
 	const untrustedGuild = await bot.guilds.fetch(config.untrusted_guild_id)
-	const member = untrustedGuild.members.cache.find(m => m.id === msg.author.id)
+	const member = await untrustedGuild.members.fetch(msg.author.id)
 	if (!member) return msg.reply({ content: 'You are not in the untrusted guild!' })
 	if (member.roles.cache.find(r => r.id === config.supporter_role_url()))
 		return msg.reply({ content: 'You already have the supporter role!' })
-	const code = args[0]
+	const [code, ign] = args
 	try {
-		const response = await (await fetch({
-			url: config.supporter_role_url(), method: "POST",
-			// @ts-ignore
-			body: JSON.stringify({ code })
+		const url = config.supporter_role_url()
+		console.log(url)
+		const response = await (await fetch(url, {
+			method: "POST",
+			body: JSON.stringify({ code, ign }),
 		})).json()
 		const isCodeValid = response[0].supporter.toLowerCase() === "yes"
 		if (!isCodeValid) return msg.reply({ content: 'Invalid supporter code!' })
@@ -24,6 +26,7 @@ const supporter = async (msg: Message, args: string[], bot: Client): Promise<Mes
 		await member.roles.add(role)
 		return msg.reply({ content: 'You have been given the supporter role!' })
 	} catch (err: any) {
+		console.log(err)
 		Log.error(`supporter.ts: ${err.message}`)
 		return msg.reply({ content: 'There was an error while executing this command!' })
 	}
