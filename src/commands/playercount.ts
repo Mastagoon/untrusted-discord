9dd77c45f63d4d4a63ab9fb373b2a192e91e8517
Mastagoon <inzilagiway@gmail.com>
2023-06-19 05:46:20 +0300

    closes #9

--- src/commands/playercount.ts
+++ src/commands/playercount.ts
@@ -17,7 +17,7 @@ const playerCount = async (options: CommandExecuteParameters) => {
     return isSlash ? interaction?.reply(response) : message?.reply(response)
   }
   // positive number, give more details
-  
+
   // Sets default text strings.
   var operativesText = `There are currently **${playerCount.Online_players} operatives online.**`;
   var lobbiesText = `There are **${playerCount.Open_lobbies} open lobbies**`;
@@ -25,17 +25,17 @@ const playerCount = async (options: CommandExecuteParameters) => {
   // Converts the each type (Online_players, Open_lobbies, Active_games) to number to get a more response. 
 
   // EG: "There are currently **1 operatives online.**" -> "**There is currently 1 operative online.**"
-  if(Number(playerCount.Online_players) == 1) operativesText = "**There is currently 1 operative online.**"; // If (1 == 1), should also = 1.
+  if (Number(playerCount.Online_players) == 1) operativesText = "**There is currently 1 operative online.**"; // If (1 == 1), should also = 1.
   // EG: "There are **1 open lobbies**" -> "There is **1 open lobby**"...
-  if(Number(playerCount.Open_lobbies) == 1) lobbiesText = "There is **1 open lobby**";
+  if (Number(playerCount.Open_lobbies) == 1) lobbiesText = "There is **1 open lobby**";
   // EG: ..."1 active games" -> "1 active game."
-  if(Number(playerCount.Active_games) == 1) activeGamesText = "**1 active game.**"
+  if (Number(playerCount.Active_games) == 1) activeGamesText = "**1 active game.**"
   //Removing the spaces creates left aligned text.
-/*
-There are currently 1 operatives online.           There are currently 1 operatives online. 
- There are 1 open lobbies and 0 active games. -->  There are 1 open lobbies and 0 active games.
-*/
-  const response = `{operativesText}\n{lobbiesText} and {activeGamesText}`; //Fully formatted.
+  /*
+  There are currently 1 operatives online.           There are currently 1 operatives online. 
+   There are 1 open lobbies and 0 active games. -->  There are 1 open lobbies and 0 active games.
+  */
+  const response = `${operativesText}\n${lobbiesText} and ${activeGamesText}`; //Fully formatted.
   return isSlash ? interaction?.reply(response) : message?.reply(response)
 }
 export default new Command(
