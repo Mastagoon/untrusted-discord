6c97c82c2d468eda9295cebad812047f7872aa7c
Arch <andrew.archer@hgsd1.com>
2023-07-16 22:51:58 -0500

    Pretty: Formating.

--- src/commands/matchmaking.ts
+++ src/commands/matchmaking.ts
@@ -6,14 +6,9 @@ const options: CommandArg[] = []
 const matchmaking = async (options: CommandExecuteParameters) => {
   const { type, message, interaction } = options
   const isSlash = type === "interaction"
-/*
-    return isSlash
-      ? interaction?.reply("This feature is currently disabled.")
-      : message?.reply("This feature is currently disabled.")
-*/  
-    var response = `<@&750427946547937323>!`;
-    return isSlash ? interaction?.reply((response + ` Pinged by ${interaction.user.username}`)) : message?.reply((response + ` Pinged by ${message.author.username}`));
-    //Discord's interaction is .user and message is .author and is quite annoying.
+  var response = `<@&750427946547937323>!`;
+  return isSlash ? interaction?.reply((response + ` Pinged by ${interaction.user.username}`)) : message?.reply((response + ` Pinged by ${message.author.username}`));
+  //Discord's interaction is .user and message is .author and is quite annoying.
 }
 export default new Command(
   "matchmaking",
